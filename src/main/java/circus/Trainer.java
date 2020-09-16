9f18b0cbbfe72e3faab869ef8c160f343d59693d
tammykoh <tammykohrw@gmail.com>
2020-09-16 20:42:36 +0800

    Add changes done in the lecture

--- src/main/java/circus/Trainer.java
+++ src/main/java/circus/Trainer.java
@@ -1,3 +1,10 @@
+package circus;
+
+import circus.animal.Animal;
+import circus.animal.Bird;
+import circus.animal.Duck;
+import circus.animal.Parrot;
+
 public class Trainer {
     public static void main(String[] args) {
         Duck d = new Duck();
@@ -11,9 +18,9 @@ public class Trainer {
 
         Duck d2 = (Duck) a; // downcasting
         train(new Duck());
-        // train(new Parrot());
-        Animal a2 = new Animal();
-        Bird b2 = new Bird();
+        train(new Parrot()); //would result in ClassCastException
+//        circus.animal.Animal a2 = new circus.animal.Animal();
+//        circus.animal.Bird b2 = new circus.animal.Bird();
     }
 
     private static void getToSpeak(Animal animal) {
@@ -21,7 +28,9 @@ public class Trainer {
     }
 
     private static void train(Bird bird) {
-        Duck d = (Duck) bird;
-        d.swim();
+        if (bird instanceof Duck) { //this handles the ClassCastException in line14
+            Duck d = (Duck) bird;
+            d.swim();
+        }
     }
 }
